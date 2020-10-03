fae3ee1ef00f5191df9a2d305e9e0b15538b3eb2
Fabian <merkisoft@gmail.com>
2020-10-03 21:11:33 +0200

    first webgl build

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MidiPlayerTK;
 using UnityEngine;
+using MidiPlayerTK;
+using UnityEngine.UI;
 
-public class Test : MonoBehaviour
-{
+public class Test : MonoBehaviour {
+    public MidiStreamPlayer player;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,22 @@ public class Test : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+    }
+    
+    public void PlayOneNote(Slider slider) {
+        var value = slider.value;
         
+        // Play a note C5 for 1 second on the channel 0
+        MPTKEvent NotePlaying = new MPTKEvent()
+        {
+            Command = MPTKCommand.NoteOn,
+            Value = (int) value,  //C5
+            Channel = 9,
+            Duration = 1000,
+            Velocity = 100,
+            Delay = 0,
+        };
+        player.MPTK_PlayEvent(NotePlaying);
     }
 }
